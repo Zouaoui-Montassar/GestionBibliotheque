a25db62881a565f8ec0a7d2e593569d44c011404
Monta <zouaouimonta12345@gmail.com>
2023-12-09 02:53:38 +0100

    App done !

--- src/App.java
+++ src/App.java
@@ -1,8 +1,31 @@
+import java.io.File;
+import Exceptions.IOException;
 import javax.swing.SwingUtilities;
+
 import swingclass.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    // nfasarlek el principe : lock file ykhalik ma texecuti lapplication ken mara khw 
+    private static final String LOCK_FILE_PATH = "app.lock"; //hedhi kima lvariable lih
+    public static void main(String[] args) throws java.io.IOException, IOException {
+        
+        File lockFile = new File(LOCK_FILE_PATH);
+
+        if (lockFile.exists()) {
+            System.out.println("Another instance is already running. Exiting.");
+            System.exit(0);
+        }
+
+        lockFile.createNewFile(); //creation mtaa el lock
+
         SwingUtilities.invokeLater(() -> new LoginForm());
+
+        lockFile.deleteOnExit(); // suppression mtaa el lock file bech najmou naawdou nexecutiwha kif tetsakr mara okhra
     }
-}
+
+
+
+
+
+        
+    }
