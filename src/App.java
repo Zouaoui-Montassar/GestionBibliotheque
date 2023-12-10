e5bb1805466ec87e730f1f393f094c2a2dd41722
Monta <zouaouimonta12345@gmail.com>
2023-12-10 03:07:35 +0100

    removed comments from App

--- src/App.java
+++ src/App.java
@@ -1,31 +1,15 @@
-import java.io.File;
-import Exceptions.IOException;
-import javax.swing.SwingUtilities;
-
-import swingclass.*;
-
+import java.io.File;import Exceptions.IOException;
+import javax.swing.SwingUtilities;import swingclass.*;
 public class App {
-    // nfasarlek el principe : lock file ykhalik ma texecuti lapplication ken mara khw 
-    private static final String LOCK_FILE_PATH = "app.lock"; //hedhi kima lvariable lih
+    private static final String LOCK_FILE_PATH = "app.lock"; 
     public static void main(String[] args) throws java.io.IOException, IOException {
-        
         File lockFile = new File(LOCK_FILE_PATH);
-
         if (lockFile.exists()) {
             System.out.println("Another instance is already running.");
             System.exit(0);
         }
-
-        lockFile.createNewFile(); //creation mtaa el lock
-
+        lockFile.createNewFile(); 
         SwingUtilities.invokeLater(() -> new LoginForm());
-
-        lockFile.deleteOnExit(); // suppression mtaa el lock file bech najmou naawdou nexecutiwha kif tetsakr mara okhra
+        lockFile.deleteOnExit(); 
     }
-
-
-
-
-
-        
     }
